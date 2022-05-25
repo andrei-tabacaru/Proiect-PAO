f0c8c600d147547f48123748efab0cdc53c18c6d
Tăbăcaru Andrei <47+andrei-tabacaru@users.noreply.github.com>
2022-05-25 22:14:11 +0300

    Finished ReadWriteService
    Finished the ReadWriteService for 4 classes, all tested and working.

--- src/project/services/ReadWriteService.java
+++ src/project/services/ReadWriteService.java
@@ -1,19 +1,27 @@
 package project.services;
 
-import project.classes.Client;
-import project.classes.Hairdresser;
-import project.classes.Manager;
+import project.classes.*;
 
 import java.io.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+import static java.time.format.DateTimeFormatter.ofLocalizedDateTime;
+import static java.time.format.FormatStyle.SHORT;
 
 public class ReadWriteService {
     private static ReadWriteService readWriteService;
     private static String clientFilePath;
     private static String hairDresserFilePath;
+    private static String beautifierFilePath;
+    private static String appointmentFilePath;
 
     private ReadWriteService() {
         clientFilePath = "src/project/csv_input/clientReportData.csv";
         hairDresserFilePath = "src/project/csv_input/hairDresserReportData.csv";
+        beautifierFilePath = "src/project/csv_input/beautifierReportData.csv";
+        appointmentFilePath = "src/project/csv_input/appointmentReportData.csv";
     }
     public static ReadWriteService getReadWriteService() {
         if (readWriteService == null)
@@ -40,15 +48,52 @@ public class ReadWriteService {
             try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(hairDresserFilePath, true))) {
                 bufferedWriter.append(((Hairdresser) ob).getName());
                 bufferedWriter.append(",");
-                bufferedWriter.append(((Hairdresser) ob).getEmail());
-                bufferedWriter.append(",");
                 bufferedWriter.append(((Hairdresser) ob).getPhoneNumber());
                 bufferedWriter.append(",");
-                bufferedWriter.append((char) ((Hairdresser) ob).getMonthlyAppointmentsCounter());
+                bufferedWriter.append(((Hairdresser) ob).getEmail());
                 bufferedWriter.append(",");
-                bufferedWriter.append((char) ((Hairdresser) ob).getPayday());
+                bufferedWriter.append(String.valueOf(((Hairdresser) ob).getSalary()));
                 bufferedWriter.append(",");
-                bufferedWriter.append((char) ((Hairdresser) ob).getSalary());
+                bufferedWriter.append(String.valueOf(((Hairdresser) ob).getPayday()));
+                bufferedWriter.append(",");
+                bufferedWriter.append(String.valueOf(((Hairdresser) ob).getMonthlyAppointmentsCounter()));
+                bufferedWriter.append("\n");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        else if(ob instanceof Beautifier){
+            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(beautifierFilePath, true))) {
+                bufferedWriter.append(((Beautifier) ob).getName());
+                bufferedWriter.append(",");
+                bufferedWriter.append(((Beautifier) ob).getPhoneNumber());
+                bufferedWriter.append(",");
+                bufferedWriter.append(((Beautifier) ob).getEmail());
+                bufferedWriter.append(",");
+                bufferedWriter.append(String.valueOf(((Beautifier) ob).getSalary()));
+                bufferedWriter.append(",");
+                bufferedWriter.append(String.valueOf(((Beautifier) ob).getPayday()));
+                bufferedWriter.append(",");
+                bufferedWriter.append(String.valueOf(((Beautifier) ob).getMonthlyAppointmentsCounter()));
+                bufferedWriter.append(",");
+                bufferedWriter.append(String.valueOf(((Beautifier) ob).getServices()));
+                bufferedWriter.append("\n");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        else if(ob instanceof Appointment){
+            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(appointmentFilePath, true))) {
+                bufferedWriter.append(String.valueOf(((Appointment) ob).getClientId()));
+                bufferedWriter.append(",");
+                bufferedWriter.append(String.valueOf(((Appointment) ob).getEmployeeId()));
+                bufferedWriter.append(",");
+                bufferedWriter.append(String.valueOf(((Appointment) ob).getCost()));
+                bufferedWriter.append(",");
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+                bufferedWriter.append(((Appointment) ob).getStartDate().format(formatter));
+                bufferedWriter.append(",");
+                bufferedWriter.append(((Appointment) ob).getEndDate().format(formatter));
                 bufferedWriter.append("\n");
             } catch (IOException ex) {
                 ex.printStackTrace();
@@ -72,6 +117,60 @@ public class ReadWriteService {
                 ex.printStackTrace();
             }
         }
+        else if(type.equals("Hairdresser")) {
+            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(hairDresserFilePath))) {
+                bufferedWriter.append("NAME");
+                bufferedWriter.append(",");
+                bufferedWriter.append("EMAIL");
+                bufferedWriter.append(",");
+                bufferedWriter.append("PHONE NUMBER");
+                bufferedWriter.append(",");
+                bufferedWriter.append("SALARY");
+                bufferedWriter.append(",");
+                bufferedWriter.append("PAYDAY");
+                bufferedWriter.append(",");
+                bufferedWriter.append("NO. OF APPOINTMENTS");
+                bufferedWriter.append("\n");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        else if(type.equals("Beautifier")) {
+            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(beautifierFilePath))) {
+                bufferedWriter.append("NAME");
+                bufferedWriter.append(",");
+                bufferedWriter.append("EMAIL");
+                bufferedWriter.append(",");
+                bufferedWriter.append("PHONE NUMBER");
+                bufferedWriter.append(",");
+                bufferedWriter.append("SALARY");
+                bufferedWriter.append(",");
+                bufferedWriter.append("PAYDAY");
+                bufferedWriter.append(",");
+                bufferedWriter.append("NO. OF APPOINTMENTS");
+                bufferedWriter.append(",");
+                bufferedWriter.append("SERVICES");
+                bufferedWriter.append("\n");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        else if(type.equals("Appointment")) {
+            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(appointmentFilePath))) {
+                bufferedWriter.append("CLIENT ID");
+                bufferedWriter.append(",");
+                bufferedWriter.append("EMPLOYEE ID");
+                bufferedWriter.append(",");
+                bufferedWriter.append("PRICE");
+                bufferedWriter.append(",");
+                bufferedWriter.append("DATA START");
+                bufferedWriter.append(",");
+                bufferedWriter.append("DATA END");
+                bufferedWriter.append("\n");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 
     public void readDataFromCsv(String type) {
@@ -81,6 +180,42 @@ public class ReadWriteService {
                 while ((record = bufferedReader.readLine()) != null) {
                     String[] recordData = record.split(",");
                     System.out.println(recordData[0] + " " + recordData[1] + " " + recordData[2] + " " + recordData[3]);
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        else if(type.equals("Hairdresser")) {
+            try (BufferedReader bufferedReader = new BufferedReader(new FileReader(hairDresserFilePath))) {
+                String record;
+                while ((record = bufferedReader.readLine()) != null) {
+                    String[] recordData = record.split(",");
+                    System.out.println(recordData[0] + " " + recordData[1] + " " + recordData[2] + " " + recordData[3]
+                            + " " + recordData[4] + " " + recordData[5]);
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        else if(type.equals("Beautifier")) {
+            try (BufferedReader bufferedReader = new BufferedReader(new FileReader(beautifierFilePath))) {
+                String record;
+                while ((record = bufferedReader.readLine()) != null) {
+                    String[] recordData = record.split(",");
+                    System.out.println(recordData[0] + " " + recordData[1] + " " + recordData[2] + " " + recordData[3]
+                            + " " + recordData[4] + " " + recordData[5] + " " + recordData[6]);
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        else if(type.equals("Appointment")) {
+            try (BufferedReader bufferedReader = new BufferedReader(new FileReader(appointmentFilePath))) {
+                String record;
+                while ((record = bufferedReader.readLine()) != null) {
+                    String[] recordData = record.split(",");
+                    System.out.println(recordData[0] + " " + recordData[1] + " " + recordData[2] + " " + recordData[3]
+                            + " " + recordData[4]);
                 }
             } catch (IOException ex) {
                 ex.printStackTrace();
--- src/project/services/Service.java
+++ src/project/services/Service.java
@@ -1,10 +1,10 @@
 package project.services;
 
-import project.classes.Appointment;
-import project.classes.Client;
-import project.classes.Employee;
+import project.classes.*;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.chrono.ChronoLocalDateTime;
 import java.util.*;
 
 public class Service {
@@ -30,42 +30,195 @@ public class Service {
                 new Client("Name 2", "0741112115", "client2@mail.com", 21)
         );
 
+        List<Hairdresser> hairdressers = Arrays.asList(
+                new Hairdresser("Name 1", "0741112113", "client1@mail.com", 1400, 15, 0),
+                new Hairdresser("Name 3", "0741112114", "client3@mail.com", 1400, 15, 0),
+                new Hairdresser("Name 2", "0741112115", "client2@mail.com", 1400, 15, 0)
+        );
 
-        boolean ok = true;
-        while(ok)
-        {
+        List<Beautifier> beautifiers = Arrays.asList(
+                new Beautifier("Name 1", "0741112113", "client1@mail.com", 1400, 15, 0, new ArrayList<String>(Arrays.asList("service0", "service1", "service2"))),
+                new Beautifier("Name 3", "0741112114", "client3@mail.com", 1400, 15, 0, new ArrayList<String>(Arrays.asList("service3", "service4", "service5"))),
+                new Beautifier("Name 2", "0741112115", "client2@mail.com", 1400, 15, 0, new ArrayList<String>(Arrays.asList("service6", "service7", "service7")))
+        );
+        LocalDateTime startDateTime = LocalDateTime.of(2014, 1, 2, 14, 0);
+        LocalDateTime startEndTime = LocalDateTime.of(2014, 1, 2, 16, 0);
+        List<Appointment> appointments = Arrays.asList(
+                new Appointment(1, 4, 200, startDateTime, startEndTime),
+                new Appointment(2, 5, 220, startDateTime, startEndTime),
+                new Appointment(3, 6, 210, startDateTime, startEndTime)
+        );
+
+        boolean okFirst = true;
+        while(okFirst) {
             System.out.println("\nAlegeti una din urmatoarele optiuni:");
-            System.out.println("1.Generare raport sortat crescator dupa nume");
-            System.out.println("2.Generare raport sortat descrescator dupa nume");
-            System.out.println("3.Generare raport normal");
-            System.out.println("4.Afiseaza raportul in consola");
+            System.out.println("1.Generare/afisare raport Client");
+            System.out.println("2.Generare/afisare raport Hairdresser");
+            System.out.println("3.Generare/afisare raport Beautifier");
+            System.out.println("4.Generare/afisare raport Appointments");
             System.out.println("5.Inchide");
             System.out.println("Alege un numar:");
 
             String line = scanner.nextLine();
             switch (line) {
                 case "1":
-                    Collections.sort(clients, Comparator.comparing(Client::getName));
-                    readWriteService.initReportHeader("Client");
-                    clients.forEach(client -> readWriteService.writeDataToReport(client));
-                    System.out.println("Report was generated with success!!!");
+                    boolean okClient = true;
+                    while (okClient) {
+                        System.out.println("\nAlegeti una din urmatoarele optiuni:");
+                        System.out.println("1.Generare raport sortat crescator dupa nume");
+                        System.out.println("2.Generare raport sortat descrescator dupa nume");
+                        System.out.println("3.Generare raport normal");
+                        System.out.println("4.Afiseaza raportul in consola");
+                        System.out.println("5.Inchide");
+                        System.out.println("Alege un numar:");
+
+                        String lineClient = scanner.nextLine();
+                        switch (lineClient) {
+                            case "1":
+                                Collections.sort(clients, Comparator.comparing(Client::getName));
+                                readWriteService.initReportHeader("Client");
+                                clients.forEach(client -> readWriteService.writeDataToReport(client));
+                                System.out.println("Report was generated with success!!!");
+                                break;
+                            case "2":
+                                Collections.sort(clients, Comparator.comparing(Client::getName).reversed());
+                                readWriteService.initReportHeader("Client");
+                                clients.forEach(client -> readWriteService.writeDataToReport(client));
+                                System.out.println("Report was generated with success!!!");
+                                break;
+                            case "3":
+                                readWriteService.initReportHeader("Client");
+                                clients.forEach(client -> readWriteService.writeDataToReport(client));
+                                System.out.println("Report was generated with success!!!");
+                                break;
+                            case "4":
+                                readWriteService.readDataFromCsv("Client");
+                                break;
+                            case "5":
+                                okClient = false;
+                                break;
+                        }
+                    }
                     break;
                 case "2":
-                    Collections.sort(clients, Comparator.comparing(Client::getName).reversed());
-                    readWriteService.initReportHeader("Client");
-                    clients.forEach(client -> readWriteService.writeDataToReport(client));
-                    System.out.println("Report was generated with success!!!");
+                    boolean okHairdresser = true;
+                    while (okHairdresser) {
+                        System.out.println("\nAlegeti una din urmatoarele optiuni:");
+                        System.out.println("1.Generare raport sortat crescator dupa nume");
+                        System.out.println("2.Generare raport sortat descrescator dupa nume");
+                        System.out.println("3.Generare raport normal");
+                        System.out.println("4.Afiseaza raportul in consola");
+                        System.out.println("5.Inchide");
+                        System.out.println("Alege un numar:");
+
+                        String lineHairdresser = scanner.nextLine();
+                        switch (lineHairdresser) {
+                            case "1":
+                                Collections.sort(hairdressers, Comparator.comparing(Hairdresser::getName));
+                                readWriteService.initReportHeader("Hairdresser");
+                                hairdressers.forEach(hairdresser -> readWriteService.writeDataToReport(hairdresser));
+                                System.out.println("Report was generated with success!!!");
+                                break;
+                            case "2":
+                                Collections.sort(hairdressers, Comparator.comparing(Hairdresser::getName).reversed());
+                                readWriteService.initReportHeader("Hairdresser");
+                                hairdressers.forEach(hairdresser -> readWriteService.writeDataToReport(hairdresser));
+                                System.out.println("Report was generated with success!!!");
+                                break;
+                            case "3":
+                                readWriteService.initReportHeader("Hairdresser");
+                                hairdressers.forEach(hairdresser -> readWriteService.writeDataToReport(hairdresser));
+                                System.out.println("Report was generated with success!!!");
+                                break;
+                            case "4":
+                                readWriteService.readDataFromCsv("Hairdresser");
+                                break;
+                            case "5":
+                                okHairdresser = false;
+                                break;
+                        }
+                    }
                     break;
                 case "3":
-                    readWriteService.initReportHeader("Client");
-                    clients.forEach(client -> readWriteService.writeDataToReport(client));
-                    System.out.println("Report was generated with success!!!");
+                    boolean okBeautifier = true;
+                    while (okBeautifier) {
+                        System.out.println("\nAlegeti una din urmatoarele optiuni:");
+                        System.out.println("1.Generare raport sortat crescator dupa nume");
+                        System.out.println("2.Generare raport sortat descrescator dupa nume");
+                        System.out.println("3.Generare raport normal");
+                        System.out.println("4.Afiseaza raportul in consola");
+                        System.out.println("5.Inchide");
+                        System.out.println("Alege un numar:");
+
+                        String lineBeautifier = scanner.nextLine();
+                        switch (lineBeautifier) {
+                            case "1":
+                                Collections.sort(beautifiers, Comparator.comparing(Beautifier::getName));
+                                readWriteService.initReportHeader("Beautifier");
+                                beautifiers.forEach(beautifier -> readWriteService.writeDataToReport(beautifier));
+                                System.out.println("Report was generated with success!!!");
+                                break;
+                            case "2":
+                                Collections.sort(beautifiers, Comparator.comparing(Beautifier::getName).reversed());
+                                readWriteService.initReportHeader("Beautifier");
+                                beautifiers.forEach(beautifier -> readWriteService.writeDataToReport(beautifier));
+                                System.out.println("Report was generated with success!!!");
+                                break;
+                            case "3":
+                                readWriteService.initReportHeader("Beautifier");
+                                beautifiers.forEach(beautifier -> readWriteService.writeDataToReport(beautifier));
+                                System.out.println("Report was generated with success!!!");
+                                break;
+                            case "4":
+                                readWriteService.readDataFromCsv("Beautifier");
+                                break;
+                            case "5":
+                                okBeautifier = false;
+                                break;
+                        }
+                    }
                     break;
                 case "4":
-                    readWriteService.readDataFromCsv("Client");
+                    boolean okAppointment = true;
+                    while (okAppointment) {
+                        System.out.println("\nAlegeti una din urmatoarele optiuni:");
+                        System.out.println("1.Generare raport sortat crescator dupa pret");
+                        System.out.println("2.Generare raport sortat descrescator dupa pret");
+                        System.out.println("3.Generare raport normal");
+                        System.out.println("4.Afiseaza raportul in consola");
+                        System.out.println("5.Inchide");
+                        System.out.println("Alege un numar:");
+
+                        String lineAppointment = scanner.nextLine();
+                        switch (lineAppointment) {
+                            case "1":
+                                Collections.sort(appointments, Comparator.comparing(Appointment::getCost));
+                                readWriteService.initReportHeader("Appointment");
+                                appointments.forEach(appointment -> readWriteService.writeDataToReport(appointment));
+                                System.out.println("Report was generated with success!!!");
+                                break;
+                            case "2":
+                                Collections.sort(appointments, Comparator.comparing(Appointment::getCost).reversed());
+                                readWriteService.initReportHeader("Appointment");
+                                appointments.forEach(appointment -> readWriteService.writeDataToReport(appointment));
+                                System.out.println("Report was generated with success!!!");
+                                break;
+                            case "3":
+                                readWriteService.initReportHeader("Appointment");
+                                appointments.forEach(appointment -> readWriteService.writeDataToReport(appointment));
+                                System.out.println("Report was generated with success!!!");
+                                break;
+                            case "4":
+                                readWriteService.readDataFromCsv("Appointment");
+                                break;
+                            case "5":
+                                okAppointment = false;
+                                break;
+                        }
+                    }
                     break;
                 case "5":
-                    ok = false;
+                    okFirst = false;
                     break;
             }
         }
